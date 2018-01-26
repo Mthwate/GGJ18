5584cca0916943dc59abfcf1220d845d936bbd6d
Matthew Benkart <Mthwaterules@gmail.com>
2018-01-26 18:53:30 -0500

    Initial commit

---
+++ Source/GGJ18/GGJ18GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GGJ18GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ18_API AGGJ18GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
